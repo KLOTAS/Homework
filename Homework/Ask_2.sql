35dfba152e267079df8f2c683643df9142779e46
KLOTAS <89389180+KLOTAS@users.noreply.github.com>
2022-01-07 15:26:18 +0300

    Add files via upload

---
+++ Homework/Ask_2.sql
@@ -0,0 +1,13 @@
+ALTER TABLE products 
+    CHANGE COLUMN `created_at` `created_at` VARCHAR(256) NULL,
+    CHANGE COLUMN `updated_at` `updated_at` VARCHAR(256) NULL;
+
+describe products;
+SELECT created_at from products;
+
+ALTER TABLE products 
+    CHANGE COLUMN `created_at` `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CHANGE COLUMN `updated_at` `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+describe products;
+SELECT created_at from products;
