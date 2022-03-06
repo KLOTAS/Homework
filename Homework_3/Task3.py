579344db18a2836f25c86a465ff45beca41064c6
KLOTAS <89389180+KLOTAS@users.noreply.github.com>
2022-03-06 19:32:33 +0300

    Delete Task3.py

--- Homework_3/Task3.py
+++
@@ -1,9 +0,0 @@
-def my_func(num_1, num_2, num_3):
-    sum_nums = num_1 + num_2 + num_3
-    return sum_nums - min((num_1, num_2, num_3))
-
-a = int(input("a: "))
-b = int(input("b: "))
-c = int(input("c: "))
-x = my_func(a, b)
-print(x)
