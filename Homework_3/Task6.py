0dbbd94db09474315162edf0f2e6322f400fd980
KLOTAS <89389180+KLOTAS@users.noreply.github.com>
2022-03-06 19:32:03 +0300

    Delete Task6.py

--- Homework_3/Task6.py
+++
@@ -1,8 +0,0 @@
-def int_func(txt):
-    word = txt[0].upper() + txt[1:].lower()
-    return word
-
-
-string = input ("Enter words separated by a space:")
-for word in string.split(' '):
-    print(f'{int_func(word)} ', end=' ')
